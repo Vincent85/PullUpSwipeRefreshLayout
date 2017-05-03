332776bc6ba85fce596b4fd0b27e16571ec64a90
562148 <vin1985@163.com>
2017-05-03 11:49:40 +0800

    提交界面截图。

--- library/src/main/java/com/nd/cbs/library/customview/LoadMoreItemDecoration.java
+++ library/src/main/java/com/nd/cbs/library/customview/LoadMoreItemDecoration.java
@@ -1,13 +1,11 @@
 package com.nd.cbs.library.customview;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -21,11 +19,16 @@ import com.nd.cbs.library.customview.util.SystemUtil;
 public class LoadMoreItemDecoration extends RecyclerView.ItemDecoration {
 
     public static final String TAG = "LoadMoreItemDecoration";
-
     private Bitmap mProgressBmp;
 
     private float mRotateDegree = 0f;
 
+    /**
+     * 可通过重写该方法提供自定义尾部视图
+     * @param c
+     * @param recyclerView
+     * @param state
+     */
     @Override
     public void onDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.State state) {
         RecyclerView.Adapter adapter = recyclerView.getAdapter();
@@ -36,7 +39,7 @@ public class LoadMoreItemDecoration extends RecyclerView.ItemDecoration {
         final int left = recyclerView.getPaddingLeft() ;
         final int right = recyclerView.getMeasuredWidth() - recyclerView.getPaddingRight() ;
         final int childSize = recyclerView.getChildCount() ;
-        final View child = recyclerView.getChildAt( childSize - 1 ) ;
+        final View child = recyclerView.getChildAt(childSize - 1) ;
         RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) child.getLayoutParams();
         final int top = child.getBottom() + layoutParams.bottomMargin ;
         final int bottom = top + SystemUtil.dip2px(recyclerView.getContext(),50) ;
@@ -45,17 +48,15 @@ public class LoadMoreItemDecoration extends RecyclerView.ItemDecoration {
         paint.setAntiAlias(true);// 抗锯齿
         paint.setFlags(Paint.ANTI_ALIAS_FLAG);// 增强消除锯齿
         paint.setStrokeWidth(3);// 设置画笔的宽度
-        paint.setTextSize(recyclerView.getContext().getResources().getDimension(R.dimen.loading_text));// 设置文字的大小
+        paint.setTextSize(recyclerView.getContext().getResources().getDimension(R.dimen.loading_text_size));// 设置文字的大小
         paint.setColor(Color.BLACK);// 设置画笔颜色
 
         String text = recyclerView.getContext().getString(R.string.pullup_loading);
-
         if(null == mProgressBmp) {
-            mProgressBmp = BitmapFactory.decodeResource(recyclerView.getResources(), R.drawable.general_load_small);
+            mProgressBmp = BitmapFactory.decodeResource(recyclerView.getResources(),R.drawable.general_load_small);
         }
-
         //转动圆圈直径
-        int diameter = mProgressBmp.getHeight();
+        int diameter = mProgressBmp.getWidth();
 
         float textWidth = paint.measureText(text,0,text.length());
         //计算文本绘制坐标
@@ -76,7 +77,6 @@ public class LoadMoreItemDecoration extends RecyclerView.ItemDecoration {
         }
         mRotateDegree += 5;
         recyclerView.invalidate(left,top,right,bottom);
-
     }
 
     @Override
@@ -87,16 +87,8 @@ public class LoadMoreItemDecoration extends RecyclerView.ItemDecoration {
         }
         int lastVisibleIndex = parent.getChildAdapterPosition(view);
         if(lastVisibleIndex == (adapter.getItemCount()-1)) {
-//            Log.d(TAG,"lastVisibleItemIndex is " + lastVisibleIndex + ",adapter last index is " + (adapter.getItemCount()-1));
-            outRect.set(0,0,0, SystemUtil.dip2px(parent.getContext(),50));
+            outRect.set(0,0,0, parent.getContext().getResources().getDimensionPixelSize(R.dimen.load_more_item_height));
         }
     }
-
-    public static int findLastVisibleItemIndex(RecyclerView.LayoutManager manager) {
-        return ((LinearLayoutManager)manager).findLastVisibleItemPosition();
-    }
-
-
-
 
 }
